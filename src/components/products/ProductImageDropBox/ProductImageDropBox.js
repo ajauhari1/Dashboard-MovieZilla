45d0deef368264eaff2eafbce9b266b85ef9eeb1
Abhishek Jauhari <65052823+ajauhari1@users.noreply.github.com>
2022-03-22 23:06:46 -0600

    removed array productimagedropbox

--- src/components/products/ProductImageDropBox/ProductImageDropBox.js
+++ src/components/products/ProductImageDropBox/ProductImageDropBox.js
@@ -8,7 +8,7 @@
         const path = acceptedFiles[0] ;
          
         setProductImage(URL.createObjectURL(path))
-      }, [])
+      })
       const {
         getRootProps,
         getInputProps,
